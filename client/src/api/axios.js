ff7cd3be879b2f043f8c7b3cf0a1269324ce484d
sarthakkeche <kechesarthak412@gmail.com>
2026-02-17 14:57:39 +0530

    axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Match your backend port
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
